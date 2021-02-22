c806be2bc51984267eb9362b6b6b5c4f0ff10e5e
Rubén Palmer Pérez <deltas.game@gmail.com>
2021-02-22 13:56:48 +0100

    Init

---
+++ bloques.c
@@ -0,0 +1,31 @@
+#include "bloques.h"
+
+static int descriptor = 0;
+
+/*
+ * Montar una unidad virtual
+ */
+int bmount(const char* path){
+
+}
+
+/*
+ * Desmontar una unidad virtual
+ */
+int bumount(){
+
+}
+
+/*
+ * Escritura de UN bloque 
+ */
+int bwrite(unsigned int nbloque, const void *buf){
+
+}
+
+/*
+ * Lectura de un bloque. B R E A D
+ */
+int bread(unsigned int nbloque, void *buf){
+
+}
