9c60a8553128b7bc87b40d05b2f073b3dd828880
JaumeJulia <Jaumejuli588@gmail.com>
2021-04-23 17:38:00 +0200

    Second method finished
    It still needs that upgrade, that optimization, that Adelaida asks for. Furthermore, maybe some calls to other methods may malfunction because of "ptr" without an &

--- C files/AuxiliarFiles/lvl6.c
+++ C files/AuxiliarFiles/lvl6.c
@@ -35,13 +35,18 @@ int liberar_inodo(unsigned int ninodo){
 int liberar_bloques_inodo(unsigned int primerBL, inodo_t inodo){
     unsigned char bufAux_punteros[BLOCKSIZE]; //1024 bytes
     unsigned int bloque_punteros[NPUNTEROS]; //1024 bytes
-    unsigned int ultimoBL, nivel_punteros, indice, ptr, nbl;
+    unsigned int ultimoBL, ptr, nbl;
+    int nRangoBL, nivel_punteros, indice;
+    int ptr_nivel[3];
+    int indices[3];
+    unsigned int bloques_punteros[3][NPUNTEROS];
+    int liberados;
 
     if (inodo.tamEnBytesLog == 0){ //el inodo esta vacio
         return 0;
     }
 
-    if (inodo.tamEnBytesLog % BLOCKSIZE == 0){
+    if (inodo.tamEnBytesLog % BLOCKSIZE == 0){ //encontramos el ultimo bloque logico
         ultimoBL = inodo.tamEnBytesLog/BLOCKSIZE - 1;
     } else {
         ultimoBL = inodo.tamEnBytesLog/BLOCKSIZE;
@@ -50,6 +55,52 @@ int liberar_bloques_inodo(unsigned int primerBL, inodo_t inodo){
     ptr = 0;
 
     for (nbl = primerBL ; nbl <= ultimoBL ; nbl++){
-        
+        if ((nRangoBL = obtener_nRangoBL(&inodo, nbl, &ptr)) < 0) {
+            return - 1;
+        }
+        nivel_punteros = nRangoBL;
+        while ((ptr > 0) && (nivel_punteros > 0)){
+            indice = obtener_indice(nbl, nivel_punteros);
+            if((indice == 0) || (nbl = primerBL)){
+                bread(ptr, bloque_punteros[nivel_punteros - 1]);//puede dar fallo por ptr
+            }
+            ptr_nivel[nivel_punteros -1] = ptr;
+            indices[nivel_punteros - 1] = indice;
+            ptr = bloques_punteros[nivel_punteros - 1][indice];
+            nivel_punteros--;
+        }
+
+        if (ptr > 0){
+            liberar_bloque(ptr); //puede dar fallo por ptr
+            liberados++;
+            if (nRangoBL == 0){
+                inodo.punterosDirectos[nbl] = 0;
+            } else {
+                nivel_punteros = 1;
+                while (nivel_punteros <= nRangoBL){
+                    indice = indices[nivel_punteros - 1];
+                    bloques_punteros[nivel_punteros -1][indice] = 0;
+                    ptr = ptr_nivel[nivel_punteros - 1];
+                    if (memcmp(bloque_punteros[nivel_punteros - 1],bufAux_punteros,BLOCKSIZE) == 0){
+                        liberar_bloque(ptr);
+                        liberados++;
+                        //aqui dice que hay que mejorar el algoritmo para saltar bloques que no hace falta explorar
+                        //pero ahora mismo ni zorra.
+                        if(nivel_punteros = nRangoBL){
+                            inodo.punterosIndirectos[nRangoBL - 1] = 0;
+                        }
+                        nivel_punteros++;
+                    } else {
+                        bwrite(ptr, bloques_punteros[nivel_punteros - 1]); //puede dar fallo por ptr
+                        nivel_punteros = nRangoBL + 1;
+                        //dice que aqui tenemos que salir del bucle porque
+                        //no es necesario liberar los bloques de niveles superiores
+                        //de los que cuelga
+                    }
+                }
+            }
+        }
+
     }
+    return liberados;
 }
